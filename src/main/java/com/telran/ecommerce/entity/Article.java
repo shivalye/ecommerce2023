bee5d6d070313fd264f8076418e018055eb7958d
Larisa Becket <lbikteeva@gmail.com>
2024-01-08 08:17:09 +0200

    Added functionality and MongoDB connection for Article entity

--- src/main/java/com/telran/ecommerce/entity/Article.java
+++ src/main/java/com/telran/ecommerce/entity/Article.java
@@ -1,37 +1,38 @@
 package com.telran.ecommerce.entity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
+import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
-import java.util.UUID;
+
 @Getter
+@Setter
 @ToString
 @EqualsAndHashCode
 @Entity
+@Document(collection = "articles")
 public class Article {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID articleId;
-    @Setter
-    private String img;
-    @Setter
+   @Id
+    private int index;
     private String title;
-    @Setter
-    private String text;
-    @Setter
+    private String author;
+    private String[] text;
+    private String[] images;
     private Date date;
+    private boolean featured;
 
-    public Article(String img, String title, String text, Date date) {
-        this.img = img;
+    public Article(int index, String title, String author, String[] text,
+                   String[] images, Date date, boolean featured)
+    {
+        this.index = index;
         this.title = title;
+        this.author = author;
         this.text = text;
+        this.images = images;
         this.date = date;
+        this.featured = featured;
     }
-
 }
